9802b76436eb3ff425c9a2e9a6dc4974ace9faad
Pawel Pekrol <ppekrol@ais.pl>
2015-02-09 09:23:43 +0100

    can resolve only one assembly at time

--- Template/ILTemplate.cs
+++ Template/ILTemplate.cs
@@ -4,62 +4,69 @@ using System.Reflection;
 
 static class ILTemplate
 {
-    static readonly Dictionary<string, bool> nullCache = new Dictionary<string, bool>();
+	static readonly Dictionary<string, bool> nullCache = new Dictionary<string, bool>();
 
-    static readonly Dictionary<string, string> assemblyNames = new Dictionary<string, string>();
-    static readonly Dictionary<string, string> symbolNames = new Dictionary<string, string>();
+	static readonly Dictionary<string, string> assemblyNames = new Dictionary<string, string>();
+	static readonly Dictionary<string, string> symbolNames = new Dictionary<string, string>();
 
-    [ThreadStatic]
-    static Dictionary<string, object> currentlyLoading; 
+	[ThreadStatic]
+	static Dictionary<string, object> currentlyLoading;
 
-    public static void Attach()
-    {
-        var currentDomain = AppDomain.CurrentDomain;
-        currentDomain.AssemblyResolve += (s, e) => ResolveAssembly(e.Name);
-    }
+	private static readonly object Locker = new object();
 
-    public static Assembly ResolveAssembly(string assemblyName)
-    {
-        if (currentlyLoading == null)
-            currentlyLoading = new Dictionary<string, object>();
-        try
-        {
-            if (currentlyLoading.ContainsKey(assemblyName))
-                return null;
-            
-            currentlyLoading.Add(assemblyName, null);
+	public static void Attach()
+	{
+		var currentDomain = AppDomain.CurrentDomain;
+		currentDomain.AssemblyResolve += (s, e) => ResolveAssembly(e.Name);
+	}
 
-            if (nullCache.ContainsKey(assemblyName))
-            {
-                return null;
-            }
+	public static Assembly ResolveAssembly(string assemblyName)
+	{
+		if (currentlyLoading == null)
+			currentlyLoading = new Dictionary<string, object>();
 
-            var requestedAssemblyName = new AssemblyName(assemblyName);
+		if (nullCache.ContainsKey(assemblyName))
+			return null;
 
-            var assembly = Common.ReadExistingAssembly(requestedAssemblyName);
-            if (assembly != null)
-            {
-                return assembly;
-            }
+		try
+		{
+			if (currentlyLoading.ContainsKey(assemblyName))
+				return null;
 
-            Common.Log("Loading assembly '{0}' into the AppDomain", requestedAssemblyName);
+			currentlyLoading.Add(assemblyName, null);
 
-            assembly = Common.ReadFromEmbeddedResources(assemblyNames, symbolNames, requestedAssemblyName);
-            if (assembly == null)
-            {
-                nullCache.Add(assemblyName, true);
+			lock (Locker)
+			{
+				if (nullCache.ContainsKey(assemblyName))
+					return null;
 
-                // Handles retargeted assemblies like PCL
-                if (requestedAssemblyName.Flags == AssemblyNameFlags.Retargetable)
-                {
-                    assembly = Assembly.Load(requestedAssemblyName);
-                }
-            }
-            return assembly;
-        }
-        finally
-        {
-            currentlyLoading.Remove(assemblyName);
-        }
-    }
+				var requestedAssemblyName = new AssemblyName(assemblyName);
+
+				var assembly = Common.ReadExistingAssembly(requestedAssemblyName);
+				if (assembly != null)
+				{
+					return assembly;
+				}
+
+				Common.Log("Loading assembly '{0}' into the AppDomain", requestedAssemblyName);
+
+				assembly = Common.ReadFromEmbeddedResources(assemblyNames, symbolNames, requestedAssemblyName);
+				if (assembly == null)
+				{
+					nullCache.Add(assemblyName, true);
+
+					// Handles retargeted assemblies like PCL
+					if (requestedAssemblyName.Flags == AssemblyNameFlags.Retargetable)
+					{
+						assembly = Assembly.Load(requestedAssemblyName);
+					}
+				}
+				return assembly;
+			}
+		}
+		finally
+		{
+			currentlyLoading.Remove(assemblyName);
+		}
+	}
 }
